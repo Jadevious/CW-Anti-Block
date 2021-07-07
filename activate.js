2c1579bae5481120a2959072b097fb33e3be1cf0
John Naughton <35263075+Johnorable@users.noreply.github.com>
2021-07-07 21:25:59 +0100

    Initial upload

---
+++ activate.js
@@ -0,0 +1,8 @@
+var element = document.querySelector('div.fEy1Z2XT');
+element.style.display = "none"
+
+var element2 = document.querySelector('html');
+element2.style.removeProperty("overflow");
+
+var element3 = document.querySelector('body');
+element3.style.removeProperty("overflow");
